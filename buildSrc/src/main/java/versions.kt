aff0ccc021c91cc8941cd21bf97a7037fa36d0a7
D10NGYANG <D10NGYANG@163.com>
2023-12-13 09:42:08 +0800

    feat: update kotlin 1.9.21；

--- buildSrc/src/main/java/versions.kt
+++ buildSrc/src/main/java/versions.kt
@@ -1,6 +1,6 @@
 // lib
 const val lib_group = "com.github.D10NGYANG"
-const val lib_ver = "0.0.5"
+const val lib_ver = "0.0.6"
 
 // android
 const val android_compile_sdk = 34
@@ -12,12 +12,12 @@ const val androidx_core_ver = "1.12.0"
 const val jetpack_lifecycle_ver = "2.6.2"
 
 // kotlin
-const val kotlin_ver = "1.9.20"
-const val ksp_ver = "1.9.20-1.0.14"
+const val kotlin_ver = "1.9.21"
+const val ksp_ver = "1.9.21-1.0.15"
 // kotlin ex
 const val kotlin_coroutines_ver = "1.7.3"
 const val kotlin_serialization_json = "1.6.2"
 
 // compose
 const val compose_ver = "androidx.compose:compose-bom:2023.10.01"
-const val compose_compiler_ver = "1.5.4"
+const val compose_compiler_ver = "1.5.6"
